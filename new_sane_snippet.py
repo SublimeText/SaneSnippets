80210567623260747163cd7a24be75f6cc55f3a7
Justin Hileman <justin@justinhileman.info>
2011-07-24 09:43:41 -0400

    code cleanup, dedupe, make work with multiple selections.

--- new_sane_snippet.py
+++ new_sane_snippet.py
@@ -10,24 +10,25 @@ $0"""
 syntax_file = os.path.join(os.getcwd(), 'SaneSnippet.tmLanguage')
 
 class NewSaneSnippetCommand(sublime_plugin.TextCommand):
-	def new_sane_snippet(self, content=None):
-		v = self.view.window().new_file()
+	def has_selection(self):
+		return any(len(region) for region in self.view.sel())
+
+	def new_sane_snippet(self, window, content=None):
+		v = window.new_file()
 		v.settings().set('default_dir', os.path.join(sublime.packages_path(), 'User'))
 		v.set_syntax_file(syntax_file)
 		v.run_command('insert_snippet', { 'contents': snippet_template.replace('$0', content or '$0') })
 		v.set_scratch(True)
 
 	def run(self, edit):
-		worked = False
-		for region in self.view.sel():
-			if (len(region)):
-				self.new_sane_snippet(self.view.substr(region))
-				worked = True
-		if not worked:
-			self.new_sane_snippet()
+		w = self.view.window()
+		if self.has_selection():
+			for region in self.view.sel():
+				if (len(region)):
+					self.new_sane_snippet(w, self.view.substr(region))
+		else:
+			self.new_sane_snippet(w)
 
 class NewSaneSnippetContextCommand(NewSaneSnippetCommand):
 	def is_enabled(self):
-		for region in self.view.sel():
-			if (len(region)):
-				return True
+		return self.has_selection()
