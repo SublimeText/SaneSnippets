7192a2083bc2d6ef9283c8783afa976016009e15
Ryan Hileman <lunixbochs@gmail.com>
2011-07-24 06:31:45 -0600

    fixed New SaneSnippet for nonstandard paths

--- new_sane_snippet.py
+++ new_sane_snippet.py
@@ -1,4 +1,5 @@
 import sublime_plugin
+import os
 
 snippet_template = """---
 description: ${1:Lorizzle}
@@ -7,11 +8,13 @@ scope:       ${3:text.plain}
 ---
 $0"""
 
+syntax_file = os.path.join(os.getcwd(), 'SaneSnippet.tmLanguage')
+
 # And a command :)
 class NewSaneSnippetCommand(sublime_plugin.WindowCommand):
     def run(self):
         v = self.window.new_file()
         v.settings().set('default_dir', 'Packages/User')
-        v.settings().set('syntax', 'Packages/SaneSnippets/SaneSnippet.tmLanguage')
+        v.set_syntax_file(syntax_file)
         v.run_command('insert_snippet', { 'contents': snippet_template })
         v.set_scratch(True)
