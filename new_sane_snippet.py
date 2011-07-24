86ed56f8c848dd20089c3b466b2ac19567ed2708
Justin Hileman <justin@justinhileman.info>
2011-07-24 09:26:10 -0400

    Improve SaneSnippet commands.
    Add command to palette.
    Add context menu entry.
    Disable context menu entry when no text is selected.
    Pre-fill snippet with selected region(s) if applicable.

--- new_sane_snippet.py
+++ new_sane_snippet.py
@@ -9,11 +9,25 @@ $0"""
 
 syntax_file = os.path.join(os.getcwd(), 'SaneSnippet.tmLanguage')
 
-# And a command :)
-class NewSaneSnippetCommand(sublime_plugin.WindowCommand):
-    def run(self):
-        v = self.window.new_file()
-        v.settings().set('default_dir', os.path.join(sublime.packages_path(), 'User'))
-        v.set_syntax_file(syntax_file)
-        v.run_command('insert_snippet', { 'contents': snippet_template })
-        v.set_scratch(True)
+class NewSaneSnippetCommand(sublime_plugin.TextCommand):
+	def new_sane_snippet(self, content=None):
+		v = self.view.window().new_file()
+		v.settings().set('default_dir', os.path.join(sublime.packages_path(), 'User'))
+		v.set_syntax_file(syntax_file)
+		v.run_command('insert_snippet', { 'contents': snippet_template.replace('$0', content or '$0') })
+		v.set_scratch(True)
+
+	def run(self, edit):
+		worked = False
+		for region in self.view.sel():
+			if (len(region)):
+				self.new_sane_snippet(self.view.substr(region))
+				worked = True
+		if not worked:
+			self.new_sane_snippet()
+
+class NewSaneSnippetContextCommand(NewSaneSnippetCommand):
+	def is_enabled(self):
+		for region in self.view.sel():
+			if (len(region)):
+				return True
