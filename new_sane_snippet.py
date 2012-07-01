d1893c5f9f6088ac3e1ca88bd058e58f5ae6a866
FichteFoll <fichtefoll2@googlemail.com>
2012-07-01 23:32:04 +0200

    PEP8 and The Other Python File

--- new_sane_snippet.py
+++ new_sane_snippet.py
@@ -1,4 +1,6 @@
-import sublime, sublime_plugin, os
+import sublime
+import sublime_plugin
+import os
 
 snippet_template = """---
 description: ${1:Lorizzle}
@@ -9,26 +11,28 @@ $0"""
 
 syntax_file = os.path.join(os.getcwd(), 'SaneSnippet.tmLanguage')
 
+
 class NewSaneSnippetCommand(sublime_plugin.TextCommand):
-	def has_selection(self):
-		return any(len(region) for region in self.view.sel())
+    def has_selection(self):
+        return any(len(region) for region in self.view.sel())
 
-	def new_sane_snippet(self, window, content=None):
-		v = window.new_file()
-		v.settings().set('default_dir', os.path.join(sublime.packages_path(), 'User'))
-		v.set_syntax_file(syntax_file)
-		v.run_command('insert_snippet', { 'contents': snippet_template.replace('$0', content or '$0') })
-		v.set_scratch(True)
+    def new_sane_snippet(self, window, content=None):
+        v = window.new_file()
+        v.settings().set('default_dir', os.path.join(sublime.packages_path(), 'User'))
+        v.set_syntax_file(syntax_file)
+        v.run_command('insert_snippet', { 'contents': snippet_template.replace('$0', content or '$0') })
+        v.set_scratch(True)
 
-	def run(self, edit):
-		w = self.view.window()
-		if self.has_selection():
-			for region in self.view.sel():
-				if (len(region)):
-					self.new_sane_snippet(w, self.view.substr(region))
-		else:
-			self.new_sane_snippet(w)
+    def run(self, edit):
+        w = self.view.window()
+        if self.has_selection():
+            for region in self.view.sel():
+                if (len(region)):
+                    self.new_sane_snippet(w, self.view.substr(region))
+        else:
+            self.new_sane_snippet(w)
+
 
 class NewSaneSnippetContextCommand(NewSaneSnippetCommand):
-	def is_enabled(self):
-		return self.has_selection()
+    def is_enabled(self):
+        return self.has_selection()
