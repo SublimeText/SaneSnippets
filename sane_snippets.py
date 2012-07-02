244720ce1752ebd29a4547ce85405ab86e3764a7
FichteFoll <fichtefoll2@googlemail.com>
2012-07-03 01:01:02 +0200

    Rework the newline handling and parsing
    - Fix the regexp not matching '\r' or '\r\n' in .sane-snippets.
    - Catch the first line break in the .sane-snippet and use it while writing the CDATA into its own lines (making it *somewhat* more human readable).
    - Use named groups and `match.groupdict()`.
    (- Also fix wrong escape pattern for CDATA ending sequences.)

--- sane_snippets.py
+++ sane_snippets.py
@@ -5,24 +5,35 @@ import re
 import xml.etree.ElementTree as etree
 from tempfile import mkstemp
 
-template      = re.compile('^---$.^(.*?)^---$.^(.*)$', re.S | re.M)
-line_template = re.compile('^(.*?):\s*(.*)$')
+template      = re.compile(r'''
+---(?P<linesep>\r\n?|\n)    # initial separator
+(?P<header>.*?)\1           # the header, and backreference to the newline
+---\1                       # another separator and a newline
+(?P<content>.*)             # the content - matches till the end of the string
+''', re.S | re.X)
+line_template = re.compile(r'^(?P<key>.*?):\s*(?P<val>.*)$')
 
 
 class ElementTreeCDATA(etree.ElementTree):
     """Subclass of ElementTree which handles CDATA blocks reasonably"""
 
-    def _write(self, file, node, encoding, namespaces):
+    def __init__(self, elem, linesep='\n', *args, **kwargs):
+        etree.ElementTree.__init__(self, elem, *args, **kwargs)
+        self.linesep = linesep
+
+    def _write(self, f, node, encoding, namespaces):
         """This method is for ElementTree <= 1.2.6"""
 
         if node.tag == '![CDATA[':
             text = node.text.encode(encoding)
             # escape ']]>' sequences by wrapping them into two CDATA sections
             # http://stackoverflow.com/questions/223652
-            text = text.replace(']]>', ']]]]><![CDATA[')
-            file.write("\n<![CDATA[%s]]>\n" % text)
+            text = text.replace(']]>', ']]]]><![CDATA[>')
+            # Windows seems to replace '\r\n' by '\n' when parsing the regexp (but using only '\n' in the regexp will fail).
+            # Also, Windows replaces any '\n' write-time by '\r\n'. '\r' works as it should.
+            f.write("%(ls)s<![CDATA[%(text)s]]>%(ls)s" % dict(text=text, ls=self.linesep))
         else:
-            etree.ElementTree._write(self, file, node, encoding, namespaces)
+            etree.ElementTree._write(self, f, node, encoding, namespaces)
 
 
 def xml_append_node(s, tag, text, **kwargs):
@@ -46,14 +57,16 @@ def snippet_to_xml(snippet):
 
 
 def parse_snippet(path, name, text):
-    """Parse a .sane-snippet and return an dict with the snippet's data"""
+    """Parse a .sane-snippet and return an dict with the snippet's data
+    May raise SyntaxError (intended) or other unintended exceptions."""
 
     snippet = {
         'path':        path,
         'name':        name,
         'description': name,
-        'tabTrigger':  None,
-        'scope':       None,
+        'tabTrigger':  '',
+        'scope':       '',
+        'linesep':     os.linesep
     }
 
     def parse_val(text):
@@ -63,19 +76,22 @@ def parse_snippet(path, name, text):
     match = template.match(text)
     if match is None:
         raise SyntaxError("Unable to parse SaneSnippet")
-    (header, content) = match.groups()
-    snippet['content'] = content
+    m = match.groupdict()
+    print m
+    snippet['content'] = m['content']
+    snippet['linesep'] = m['linesep']
+    print repr(text)
 
-    for line in header.splitlines():
+    for line in m['header'].splitlines():
         match = line_template.match(line)
         if match is None:
             raise SyntaxError("Unable to parse SaneSnippet header")
-        (key, val) = match.groups()
-        key = key.strip()
-        if key in ('description', 'tabTrigger', 'scope'):
-            snippet[key] = parse_val(val)
+        m = match.groupdict()
+        m['key'] = m['key'].strip()
+        if m['key'] in ('description', 'tabTrigger', 'scope'):
+            snippet[m['key']] = parse_val(m['val'])
         else:
-            raise SyntaxError('Unexpected SaneSnippet property: "%s"' % key)
+            raise SyntaxError('Unexpected SaneSnippet property: "%s"' % m['key'])
 
     return snippet
 
@@ -113,7 +129,7 @@ def regenerate_snippet(path, onload=False):
 
         # print 'Writing SaneSnippet "%s" to "%s"' % (snippet['description'], path)
         # TODO: Prettify the XML structure before writing
-        ElementTreeCDATA(snippet_to_xml(snippet)).write(f)
+        ElementTreeCDATA(snippet_to_xml(snippet), linesep=snippet['linesep']).write(f)
 
     finally:
         f.close()
@@ -125,7 +141,7 @@ def regenerate_snippets(root=sublime.packages_path(), onload=False):
     for root, dirs, files in os.walk(root):
         for basename in files:
             # Remove old snippets
-            # TODO: Only regenerate if "previous" file's contents are equal
+            # TODO: Only regenerate if "previous" file's contents are not equal
             if basename.endswith('.sane.sublime-snippet'):
                 path = os.path.join(root, basename)
                 try:
