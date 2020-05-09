78ffed096bca732d42080d0bb72e3058f1dd5cc1
FichteFoll <fichtefoll2@googlemail.com>
2020-05-09 04:04:32 +0200

    Remove .no-sublime-package hack
    And some other minor updates to the "new sane snippet" command.

--- new_sane_snippet.py
+++ new_sane_snippet.py
@@ -1,6 +1,7 @@
+import os
+
 import sublime
 import sublime_plugin
-import os
 
 snippet_template = """---
 description: ${1:Lorizzle}
@@ -10,12 +11,7 @@ scope:       ${3:%s}
 %s"""
 
 # Should be "SaneSnippets", but do not rely on it
-PACKAGE_NAME = os.path.basename(os.path.dirname(os.path.abspath(__file__)))
-SYNTAX_FILE  = 'Packages/%s/SaneSnippet.tmLanguage' % PACKAGE_NAME
-
-
-def view_has_selection(view):
-    return any(len(region) for region in view.sel())
+SYNTAX_FILE  = 'Packages/{}/SaneSnippet.tmLanguage'.format(__package__)
 
 
 class NewSaneSnippetCommand(sublime_plugin.TextCommand):
@@ -25,30 +21,29 @@ class NewSaneSnippetCommand(sublime_plugin.TextCommand):
     def new_sane_snippet(self, window, content=None, scope=None):
         v = window.new_file()
         v.set_syntax_file(SYNTAX_FILE)
-        content = content.replace("$", "\$") if content else '$0'
+        content = content.replace("$", R"\$") if content else '$0'
         v.run_command('insert_snippet', {'contents': snippet_template % (scope or 'text.plain', content)})
 
         # Default settings
         s = v.settings()
         s.set('default_dir', os.path.join(sublime.packages_path(), 'User'))
-        s.set('translate_tabs_to_spaces', False)  # Use tabs because they are translated if necessary
 
     def run(self, edit):
         v = self.view
         w = v.window()
-        if view_has_selection(v):
+        if any(v.sel()):
             for region in v.sel():
                 if len(region):
                     scope = v.scope_name(region.begin())
                     # Strip the last scope (if there are more than 1)
-                    scope = scope.rsplit(' ', 1)[0]
+                    scope = scope.strip().rsplit(' ', 1)[0]
                     self.new_sane_snippet(w, v.substr(region), scope)
         else:
             self.new_sane_snippet(w)
 
 
 class NewSaneSnippetContextCommand(NewSaneSnippetCommand):
-    """NewSaneSnippetCommand for menus (here: context menu), with is_enabled()"""
+    """NewSaneSnippetCommand with is_visible()"""
 
-    def is_enabled(self):
-        return view_has_selection(self.view)
+    def is_visible(self):
+        return any(self.view.sel())
